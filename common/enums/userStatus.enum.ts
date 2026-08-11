412c3a92138dfb2fcb9d4f73e5cbc38143d1e276
Andrew LUTAAYA <CAL1169514+totalen@admin.hubtotal.net>
2026-08-11 18:06:40 +0300

    Using lowercase for the enums

--- common/enums/userStatus.enum.ts
+++ common/enums/userStatus.enum.ts
@@ -1,6 +1,6 @@
 export enum AccountStatus {
-  ACTIVE = 'ACTIVE',
-  INACTIVE = 'INACTIVE',
-  SUSPENDED = 'SUSPENDED',   
-  PENDING_RESET = 'PENDING_RESET', // My plan here is the manadatory resetsof passwords for a given set of time. This will help with security
+  ACTIVE = 'active',
+  INACTIVE = 'inactive',
+  SUSPENDED = 'suspended',   
+  PENDING_RESET = 'pending_reset'
 }
